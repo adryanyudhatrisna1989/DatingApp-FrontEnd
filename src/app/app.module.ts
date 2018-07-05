71dbe71bb964b5735c0a07d015581090f797d347
iamadryan <ayudhatrisna1989@gmail.com>
2018-07-05 22:13:01 +0700

    Added registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,32 +2,17 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { ValueComponent } from "./value/value.component";
 import { HttpModule } from "@angular/http";
 import { FormsModule } from "@angular/forms";
 import { NavComponent } from "./nav/nav.component";
 import { AuthService } from "./_services/auth.service";
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from "./home/home.component";
+import { RegisterComponent } from "./register/register.component";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      ValueComponent,
-      NavComponent,
-      HomeComponent,
-      RegisterComponent
-   ],
-   imports: [
-      BrowserModule,
-      HttpModule,
-      FormsModule
-   ],
-   providers: [
-      AuthService
-   ],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
+  imports: [BrowserModule, HttpModule, FormsModule],
+  providers: [AuthService],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
