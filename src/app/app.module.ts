14ebf20dabee14aeaff126a766edd9428461fe73
iamadryan <ayudhatrisna1989@gmail.com>
2018-07-06 19:19:33 +0700

    Added third party to the client side

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BsDropdownModule } from "ngx-bootstrap";
 
 import { AppComponent } from "./app.component";
 import { HttpModule } from "@angular/http";
@@ -8,11 +9,12 @@ import { NavComponent } from "./nav/nav.component";
 import { AuthService } from "./_services/auth.service";
 import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
+import { AlertifyService } from "./_services/alertify.service";
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpModule, FormsModule],
-  providers: [AuthService],
+  imports: [BrowserModule, HttpModule, FormsModule, BsDropdownModule.forRoot()],
+  providers: [AuthService, AlertifyService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
